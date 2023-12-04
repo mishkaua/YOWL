2048804406fa808a0d69b5e288b16172447b6586
Razmo <you@example.com>
2023-12-04 08:31:28 +0100

    ADDED: login seems to work

--- yowl/config/cors.php
+++ yowl/config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:8001')],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
--- yowl/routes/api.php
+++ yowl/routes/api.php
@@ -16,9 +16,15 @@ use Illuminate\Support\Facades\Route;
 /* Route::post('/login', [AuthenticatedSessionController::class, 'store'])
                 
                 ->name('login'); */
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+/* Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-Route::post('/login', function(Request $request) {
+}); */
+/* Route::post('/login', function(Request $request) {
     return response()->json($request);
+}); */
+
+Route::middleware(['auth:sanctum'])->group(function() {
+    Route::get('user', function (Request $request){
+        return $request->user();
+  });  
 });
